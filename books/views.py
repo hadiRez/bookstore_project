e1eb3680c160baaf63a2237a736444d0b4c70359
unknown <r.hadi111@gmail.com>
2022-06-27 12:25:48 +0430

    add CreateBookView

--- books/views.py
+++ books/views.py
@@ -4,6 +4,7 @@ from django.views import generic
 from .models import Book
 
 
+
 class BookListView(generic.ListView):
     model = Book
     template_name = 'books/book_list.html'
@@ -13,5 +14,11 @@ class BookListView(generic.ListView):
 class BookDetailView(generic.DetailView):
     model = Book
     template_name = 'books/book_detail.html'
+
+
+class BookCreateView(generic.CreateView):
+    model = Book
+    fields = ['title', 'author', 'description', 'price']
+    template_name = 'books/book_create.html'
 
 
