8f0f031ff6aff15a0e20e3d719430d7751a60eb1
unknown <r.hadi111@gmail.com>
2022-07-01 19:30:17 +0430

    add media config to project

--- config/settings.py
+++ config/settings.py
@@ -151,3 +151,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 # static files config
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
+
+# Unit 156
+# media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = str(BASE_DIR.joinpath('media'))
